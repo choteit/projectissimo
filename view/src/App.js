c3d3e73877db007ade9b5ac37a94721bcc4a0726
choteit <camille.hoteit@soprasteria.com>
2020-11-26 23:54:11 +0100

    Begin creation of Projects list page

--- view/src/App.js
+++ view/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Home} from './pages/home/home';
+import {ProjectsList} from './pages/projectsList/projectsList'
 
 function App() {
-  return (
-      <div>
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-            </Switch>
-          </div>
-        </Router>
-      </div>
-  );
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                    </Switch>
+                    <Switch>
+                        <Route exact path="/projects" component={ProjectsList}/>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
